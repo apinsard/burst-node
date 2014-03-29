05315d7f2a43b7ea0ac11b5d5db99160b9a36f0f
Antoine Pinsard <antoine.pinsard@gmail.com>
2014-03-29 21:24:38 +0100

    Models structure example

--- examples/structure/core/models/News.js
+++ examples/structure/core/models/News.js
@@ -4,7 +4,42 @@
 //var models = burst.core.models;
 
 var News = {
-  
+
+  title: {
+    type: 'text',
+    max_length: 50,
+    min_length: 6,
+  },
+
+  slug: {
+    type: 'slug',
+    max_length: 50,
+    min_length: 6,
+    slugify: 'title',
+  }
+
+  author: {
+    type: 'text',
+    max_length: 30,
+    min_length: 3,
+    allow_null: true,
+  },
+
+  date: {
+    type: 'date',
+  },
+
+  content: {
+    type: 'text',
+  },
+
+  views: {
+    type: 'integer',
+    min: 0,
+    max: 1e9,
+    initial: 0,
+  }
+
 };
 
 module.exports = News;
